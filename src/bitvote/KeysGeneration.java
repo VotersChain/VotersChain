651af068a5a1756a7944354dd3699c59b02d7a06
Édi Marques Aires <a33734@ubi.pt>
2018-05-08 22:17:25 +0100

    edi

---
+++ src/bitvote/KeysGeneration.java
@@ -0,0 +1,47 @@
+
+package bitvote;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.spec.ECGenParameterSpec;
+
+/**
+ *
+ * @author Asus
+ */
+
+// Geração de par de chaves com Curvas Elipticas
+
+public final class KeysGeneration
+{
+	public KeyPair keyPar;
+	
+	public KeyPair generateKeys()
+	{
+		try
+		{
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDSA","BC");
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+			ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256r1");
+			
+			//Inicializa o gerador de chaves e gera um par de chaves
+			keyGen.initialize(ecSpec, random);
+			KeyPair keyPar = keyGen.genKeyPair();
+			
+			return keyPar;
+			
+			/*
+			privateKey = keyPair.getPrivate()
+			publicKey = keyPair.getPublic()
+			*/
+								
+		}
+		catch(Exception e)
+		{
+			throw new RuntimeException(e);
+		}	
+	}
+	
+}
+
