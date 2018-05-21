0262016c7aadd0d1eb41382c73ddc0d1242db37d
Vitor <Vitor@192.168.1.69>
2018-05-21 19:07:34 +0100

    Cliente (Atualizado)

--- src/server/Client.java
+++ src/server/Client.java
@@ -5,45 +5,284 @@
  */
 package server;
 
+import bitvote.AES;
 import bitvote.SignatureUtils;
 import bitvote.StringUtils;
-import java.net.InetAddress;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.DatatypeConverter;
 
 /**
  *
  * @author jferr
  */
 public class Client {
-    
+
     private static final int PORT = 2019;
-    
-    public static void main(String[] args) {
-        try{
-            // Make reference to SSL-based registry
-            Registry registry = LocateRegistry.getRegistry("DESKTOP-C38TKIF", PORT, new RMISSLClientSocketFactory());
-            
-            
-            // "obj" is the identifier that we'll use to refer
-            // to the remote object that implements the "Hello"
-            // interface
-            Server obj = (Server) registry.lookup("Server");
+
+    public static int Registo(Server obj) {
+        try {
+            //Pedir o Nome e o ID ao Utilizador
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Nome: ");
+            String sNome = in.readLine();
+            System.out.print("Identificador: ");
+            long lID = Long.parseLong(in.readLine());
+
+            //(Cliente  -> Servidor) Nome - Id
+            //(Servidor -> Cliente ) Chaves pk, sk
             String chaves = "blank";
-            chaves = obj.regist("Tó Brito",14679456732457L);
+            chaves = obj.regist(sNome, lID);
 
-            String[] pk = chaves.split(",");
+            //Se utilizador já se encontra registado
+            if (chaves.equals("")) {
+                System.out.println("Utilizador já se encontra registado! Por favor, efetue o Login!");
+                return 1;
+            }
 
-            String nonce = obj.loginStepOne(pk[0]);
-            
-            byte[] signNonce = SignatureUtils.signString(nonce, StringUtils.getPrivateKeyFromString(pk[1]));
-            
+            //Efetuar registo
+            String[] keys = chaves.split(",");
+            String pk, sk;
+            pk = keys[0];
+            sk = keys[1];
+            System.out.println("SK: " + sk);
+
+            //Gravar num ficheiro
+            String fpk = "pk.txt";
+
+            BufferedWriter bw = null;
+            FileWriter fw = null;
+
+            //Gravar pk
+            fw = new FileWriter(fpk);
+            bw = new BufferedWriter(fw);
+            bw.write(pk);
+            bw.close();
+            fw.close();
+
+            //Gravar sk
+            byte[] iv = AES.generateIV();;
+            byte[] key = DatatypeConverter.parseHexBinary(obj.requestKey());
+
+            FileOutputStream fout = new FileOutputStream("iv_sk.txt");
+            String ivHex = DatatypeConverter.printHexBinary(iv);
+            fout.write(ivHex.getBytes());
+
+            byte[] cripto = AES.encrypt(sk, key, iv);
+            fout = new FileOutputStream("sk.txt");
+            byte[] encoded = Base64.getEncoder().encode(cripto);
+            fout.write(encoded);
+            fout.close();
+
+            //(Servidor -> Cliente ) nonce
+            String nonce = obj.loginStepOne(pk);
+
+            //(Cliente  -> Servidor) Assinar o nonce encia
+            byte[] signNonce = SignatureUtils.signString(nonce, StringUtils.getPrivateKeyFromString(sk));
+
+            //(Servidor -> Cliente ) Valida a assinatura
             boolean isLoged = obj.loginStepTwo(signNonce);
-            
-            System.out.println(isLoged);
 
+            if (isLoged) {
+                //Registado e evolui para menu 2
+                return 2;
+            } else {
+                //Erro na assinutura do nonce
+                System.out.println("Erro no registo! Por favor tente novamente!");
+                return 3;
+            }
+        } catch (RemoteException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchAlgorithmException | NoSuchProviderException | InvalidKeySpecException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
-        catch (Exception e) {
+        return 0;
+    }
+
+    public static String getSK(Server obj) {
+        String pt = "";
+        String outString = "";
+        try {
+            InputStream inputStream = new FileInputStream("sk.txt");
+            Reader inputStreamReader = new InputStreamReader(inputStream, "ISO-8859-1");
+
+            int data = inputStreamReader.read();
+            while (data != -1) {
+                char theChar = (char) data;
+                outString += theChar;
+                data = inputStreamReader.read();
+            }
+            inputStreamReader.close();
+        } catch (Exception ex) {
+            Logger.getLogger(SQLiteBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            String ivString = "";
+            FileReader filer = new FileReader("iv_sk.txt");
+            char[] buff = new char[1];
+            while (filer.read(buff) > 0) {
+                ivString += buff[0];
+            }
+            filer.close();
+
+            byte[] iv = DatatypeConverter.parseHexBinary(ivString);
+            byte[] key = DatatypeConverter.parseHexBinary(obj.requestKey());
+
+            byte[] decoded = Base64.getDecoder().decode(outString.getBytes("ISO-8859-1"));
+            pt = AES.decrypt(decoded, key, iv);
+            FileOutputStream out = new FileOutputStream("sk.txt");
+            out.write(pt.getBytes("ISO-8859-1"));
+            out.close();
+
+            //Voltar a encriptar o ficheiro
+            FileOutputStream fout = new FileOutputStream("iv_sk.txt");
+            String ivHex = DatatypeConverter.printHexBinary(iv);
+            fout.write(ivHex.getBytes());
+
+            byte[] cripto = AES.encrypt(pt, key, iv);
+            fout = new FileOutputStream("sk.txt");
+            byte[] encoded = Base64.getEncoder().encode(cripto);
+            fout.write(encoded);
+            fout.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return pt;
+    }
+
+    public static int Login(Server obj) {
+        try {
+            //Ler a pk do ficheiro
+            String fpk = "pk.txt";
+            BufferedReader br = null;
+            FileReader fr = null;
+            fr = new FileReader(fpk);
+            br = new BufferedReader(fr);
+            String pk = br.readLine();
+            br.close();
+            fr.close();
+
+            //(Servidor -> Cliente ) nonce
+            String nonce = obj.loginStepOne(pk);
+
+            //Ler o sk e decifrar
+            String sk = getSK(obj);
+
+            //(Cliente  -> Servidor) Assinar o nonce encia
+            byte[] signNonce = SignatureUtils.signString(nonce, StringUtils.getPrivateKeyFromString(sk));
+
+            //(Servidor -> Cliente ) Valida a assinatura
+            boolean isLoged = obj.loginStepTwo(signNonce);
+            if (isLoged) {
+                //Registado e evolui para menu 2
+                return 2;
+            } else {
+                //Erro na assinutura do nonce
+                System.out.println("Erro no registo! Por favor tente novamente!");
+                return 3;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return 0;
+    }
+
+    public static void menu2(Server obj) {
+        String menu2 = "***********************************\n1-Votar\n2-Status Eleição\n0-Sair\n-->";
+
+        int sair = 0;
+        while (sair == 0) {
+            System.out.print(menu2);
+            Scanner in = new Scanner(System.in);
+            int op = in.nextInt();
+            switch (op) {
+                case 1: {
+                    
+                }
+                break;
+                case 2: {
+
+                }
+                break;
+                case 0: {
+                    sair = 1;
+                }
+                break;
+                default: {
+                    System.out.println("Opção inválida tente novamente!");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            // Make reference to SSL-based registry - DESKTOP-C38TKIF
+            Registry registry = LocateRegistry.getRegistry("localhost", PORT, new RMISSLClientSocketFactory());
+            Server obj = (Server) registry.lookup("Server");
+
+            String menu1 = "***********************************\n1-Login\n2-Registar\n0-Sair\n-->";
+
+            int sair = 0;
+            while (sair == 0) {
+                System.out.print(menu1);
+                Scanner in = new Scanner(System.in);
+                int op = in.nextInt();
+                switch (op) {
+                    case 1: {
+                        int res = Login(obj);
+                        if (res == 2) {
+                            //Registado - Passar ao menu 2
+                            System.out.println("Registado!");
+                            menu2(obj);
+                            sair = 1;
+                        }
+                    }
+                    break;
+                    case 2: {
+                        int res = Registo(obj);
+                        if (res == 2) {
+                            //Registado - Passar ao menu 2
+                            System.out.println("Registado!");
+                            menu2(obj);
+                            sair = 1;
+                        }
+                    }
+                    break;
+                    case 0: {
+                        sair = 1;
+                    }
+                    break;
+                    default: {
+                        System.out.println("Opção inválida tente novamente!");
+                    }
+                }
+            }
+        } catch (Exception e) {
             System.out.println("HelloClient exception: " + e.getMessage());
             e.printStackTrace();
         }
