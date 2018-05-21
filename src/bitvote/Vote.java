e92378c3c0fb380e26b0402ddf77add6168e76e9
PAULO NETO <paulo.miguel.neto@ubi.pt>
2018-05-21 14:18:52 +0100

    StringUtils

--- src/bitvote/Vote.java
+++ src/bitvote/Vote.java
@@ -13,7 +13,7 @@ import java.security.PublicKey;
  * @author jferr
  */
 public class Vote {
-    
+    public String voteId; //contém o hash do voto
     public PublicKey sender;
     public long candidateNonce;
     public int numberVotes;
@@ -26,16 +26,39 @@ public class Vote {
         this.candidateNonce = to;
         this.numberVotes = value;
     }
+
+    Vote(PublicKey publicKey, PublicKey publicKey0, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
-   public void generateSignature(PrivateKey privateKey) throws Exception{
-       String data = StringUtils.getStringFromKey(sender) + Long.toString(candidateNonce) + Integer.toString(numberVotes);
-       signature = SignatureUtils.signString(data, privateKey);
-   }
+    public void generateSignature(PrivateKey privateKey) throws Exception{
+        String data = StringUtils.getStringFromKey(sender) + Long.toString(candidateNonce) + Integer.toString(numberVotes);
+        signature = SignatureUtils.signString(data, privateKey);
+    }
+
+    public boolean verifySignature() throws Exception{
+        String data = StringUtils.getStringFromKey(sender) + Long.toString(candidateNonce) + Integer.toString(numberVotes); 
+        return SignatureUtils.verifyString(data, signature, sender);
+    }
+    
+    public boolean processVote() throws Exception{
+        if(verifySignature()==false){
+            System.out.println("Assinatura do Voto na Verificada");
+            return false;
+        }
+        
+        voteId = calculateHash();
+       
+        return true; 
+    }
    
-   public boolean verifySignature() throws Exception{
-       String data = StringUtils.getStringFromKey(sender) + Long.toString(candidateNonce) + Integer.toString(numberVotes); 
-       return SignatureUtils.verifyString(data, signature, sender);
+   private String calculateHash(){
+       id++;
+       return HashUtils.hashFuncSHA256(StringUtils.getStringFromKey(sender) + 
+               Long.toString(candidateNonce)+
+               Integer.toString(numberVotes) + id
+               );
    }
    
    
